86da82b5c02d29ddf116f7e1fc1498bdad8d2919
Sakshi Karhade <sakshi.karhade12@gmail.com>
2024-05-07 16:19:57 +0530

    change the model and js

---
+++ eGym/Content/Notice.js
@@ -0,0 +1,32 @@
+﻿var SaveNoticeInfo = function () {
+    debugger;
+
+    var id = $("#HdnId").val();
+    var title = $("#txtFull_Name").val();
+    var description = $("#txtDescription").val();
+    var createdate = $("#HdnCreatedate").val();
+    var isActive = $("#HdnisActive").val();
+   
+    var model = {
+
+        Id:id,
+        Title: title,
+        Description: description,
+        Createdate: createdate,
+        IsActive: isActive,
+    };
+
+    $.ajax({
+        url: "/Notice/SaveNoticeInfo",
+        method: "Post",
+        data: JSON.stringify(model),
+        contentType: "Application/json;charset=utf-8",
+        dataType: "json",
+        success: function (response) {
+            alert(response.Message);
+        },
+        error: function (response) {
+            alert(response.Message);
+        }
+    });
+}
---
+++ eGym/Models/NoticeModel.cs
@@ -0,0 +1,41 @@
+﻿using eGym.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eGym.Models
+{
+    public class NoticeModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Nullable<System.DateTime> Createdate { get; set; }
+        public Nullable<bool> isActive { get; set; }
+
+        public string SaveNoticeInfo(NoticeModel model)
+        {
+            string msg = "Data Save Successfully";
+
+            GymEntities Db = new GymEntities();
+
+
+            {
+                var infoData = new tblNotice()
+                {
+                    Id = model.Id,
+                    Title = model.Title,
+                    Description = model.Description,
+                    Createdate = model.Createdate,
+                    isActive = model.isActive,
+                };
+
+                Db.tblNotices.Add(infoData);
+                Db.SaveChanges();
+                return msg;
+            }
+        }
+
+    }
+}
